15199a846d7d090a38cf6aa18967a96de4536e83
Gabriel <gabriel.fauquembergue@gmail.com>
2023-11-05 13:41:30 +0100

    touches finales

--- contact.php
+++ contact.php
@@ -1,4 +1,4 @@
-<?php
+<?php // Fichier permettant d'envoyer un mail à l'adresse de Yann avec les bonnes informations
 
 // On vérifie qu'un formulaire a été soumis en accédant à cette page
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
